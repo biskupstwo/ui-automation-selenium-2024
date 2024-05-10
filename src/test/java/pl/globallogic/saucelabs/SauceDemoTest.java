b5178e33ad65e98c9fff64ece47af025e538809d
Jan Biskupski <jbiskupski0@outlook.com>
2024-05-10 11:51:55 +0200

    added tests: shouldPersistCheckoutDetails

--- src/test/java/pl/globallogic/saucelabs/SauceDemoTest.java
+++ src/test/java/pl/globallogic/saucelabs/SauceDemoTest.java
@@ -181,5 +181,64 @@ public class SauceDemoTest {
         driver.quit();
     }
     //shouldAddListOfItemsToTheCart
-    //shouldPersistCheckoutDetails
+    @Test
+    public void shouldPersistCheckoutDetails(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+
+        username.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+        loginButton.click();
+
+        WebElement backpackAddToCart = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        backpackAddToCart.click();
+
+        WebElement cartLink = driver.findElement(By.className("shopping_cart_link"));
+        cartLink.click();
+
+        String expectedUrl = "https://www.saucedemo.com/cart.html";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+
+        WebElement checkoutBtn = driver.findElement(By.id("checkout"));
+        checkoutBtn.click();
+        //String itemPrice = inventoryItemPrice.getText();
+
+        String expectedCheckoutStepOneUrl = "https://www.saucedemo.com/checkout-step-one.html";
+        String actualUrlStep2 = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrlStep2, expectedCheckoutStepOneUrl);
+
+
+        WebElement firstName = driver.findElement(By.id("first-name"));
+        WebElement lastName = driver.findElement(By.id("last-name"));
+        WebElement postalCode = driver.findElement(By.id("postal-code"));
+
+        firstName.sendKeys("John");
+        lastName.sendKeys("Doe");
+        postalCode.sendKeys("66666");
+
+        WebElement continueBtn = driver.findElement(By.id("continue"));
+        continueBtn.click();
+
+        String expectedCheckoutStepTwoUrl = "https://www.saucedemo.com/checkout-step-two.html";
+        String actualUrlStep3 = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrlStep3, expectedCheckoutStepTwoUrl);
+
+        String backpackPrice = "$29.99";
+        WebElement inventoryItemPrice = driver.findElement(By.className("inventory_item_price"));
+        String itemPrice = inventoryItemPrice.getText();
+        Assert.assertEquals(backpackPrice, itemPrice);
+
+        WebElement subTotalPriceActualElement = driver.findElement(By.className("inventory_item_price"));
+        String subTotalPriceActual = subTotalPriceActualElement.getText();
+        Assert.assertEquals(subTotalPriceActual, backpackPrice);
+
+        driver.quit();
+    }
 }
+
